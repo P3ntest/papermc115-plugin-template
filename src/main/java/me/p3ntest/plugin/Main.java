e3d3dc3e3afc665c7b2e4852fb42f84fac0d3d01
P3ntest <juliusvanvoorden@outlook.com>
2020-05-23 16:40:39 +0200

    Initial Commit

---
+++ src/main/java/me/p3ntest/plugin/Main.java
@@ -0,0 +1,11 @@
+package me.p3ntest.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+
+}
